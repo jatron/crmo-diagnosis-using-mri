f7a399b7956e66b77cd8ba59087d1cd7c0e36eb2
Anna Merkoulovitch <anna.merkoulovitch@helix.com>
2018-11-24 21:12:06 -0800

    Moved shared functions to utils.py
    1) Added function to grid search
    2) Added a function to plot confusion matrix and print error rates

--- src/utils.py
+++ src/utils.py
@@ -3,35 +3,80 @@ import numpy as np
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
-
+import seaborn as sn
 from sklearn import model_selection
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.svm import SVC
+
 
 def generate_validation_curve(estimator, X, y, param_name, param_range, cv,
     scoring, n_jobs, title, xlabel):
 
-  train_scores, test_scores = model_selection.validation_curve(
-    estimator, X, y, param_name=param_name, param_range=param_range,
-    cv=cv, scoring=scoring, n_jobs=n_jobs)
+    train_scores, test_scores = model_selection.validation_curve(
+        estimator, X, y, param_name=param_name, param_range=param_range,
+        cv=cv, scoring=scoring, n_jobs=n_jobs)
 
-  train_scores_mean = np.mean(train_scores, axis=1)
-  train_scores_std = np.std(train_scores, axis=1)
-  test_scores_mean = np.mean(test_scores, axis=1)
-  test_scores_std = np.std(test_scores, axis=1)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
 
-  plt.title(title)
-  plt.xlabel(xlabel)
-  plt.ylabel("Accuracy")
-  plt.ylim(0.0, 1.1)
-  lw = 2
-  plt.semilogx(param_range, train_scores_mean, label="Training accuracy",
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel("Accuracy")
+    plt.ylim(0.0, 1.1)
+    lw = 2
+    plt.semilogx(param_range, train_scores_mean, label="Training accuracy",
                color="darkorange", lw=lw)
-  plt.fill_between(param_range, train_scores_mean - train_scores_std,
+    plt.fill_between(param_range, train_scores_mean - train_scores_std,
                    train_scores_mean + train_scores_std, alpha=0.2,
                    color="darkorange", lw=lw)
-  plt.semilogx(param_range, test_scores_mean, label="Cross-validation accuracy",
+    plt.semilogx(param_range, test_scores_mean, label="Cross-validation accuracy",
                color="navy", lw=lw)
-  plt.fill_between(param_range, test_scores_mean - test_scores_std,
+    plt.fill_between(param_range, test_scores_mean - test_scores_std,
                    test_scores_mean + test_scores_std, alpha=0.2,
                    color="navy", lw=lw)
-  plt.legend(loc="best")
-  plt.show()
+    plt.legend(loc="best")
+    plt.show()
+
+def plot_confusion_matrix(y_test,y_pred):
+    df_cm = confusion_matrix(y_test,y_pred)#,labels=["I", "S", "R"])
+    sn.set(font_scale=1.4)#for label size
+    sn.heatmap(df_cm, annot=True,annot_kws={"size": 16})
+
+def do_CV(X,y, model, multi_class=True):
+    # Change to 2-class
+    if not multi_class:
+        y = y.replace('S', 'R')
+    # Split the dataset in two equal parts
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=0)
+
+    print("# Tuning hyper-parameter")
+    print()
+
+    model.fit(X_train, y_train)
+
+    print("Best parameters set found on development set:")
+    print()
+    print(model.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    means = model.cv_results_['mean_test_score']
+    stds = model.cv_results_['std_test_score']
+    for mean, std, p in zip(means, stds, model.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, p))
+    print()
+
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, model.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    plot_confusion_matrix(y_true, y_pred)
+    print()
